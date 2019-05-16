a9e6a01c1fa9970a1869a07541e982fbbf77eb51
Remco Hofman <remcohofman98@gmail.com>
2019-05-16 11:31:47 +0200

    Usage of entry_points instead of scripts option

--- setup.py
+++ setup.py
@@ -18,5 +18,9 @@ This analysis leads to the generation of a report, according to a technical deta
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
-    scripts=['androwarn.py']
+    entry_points={
+        'console_scripts': [
+            'androwarn=bin.__main__:main'
+        ]
+    }
 )
